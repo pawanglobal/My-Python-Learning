5152f69d73116f4abcde9c4baa45cd0bed792fec
PAWAN KUMAR <pawanglobalO7@gmail.com>
2022-08-09 23:43:54 +0200

    Printing,Printing...(Next line \n)

---
+++ ex9.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFed\nMar\nApr\n\May\nJun\nJul\nAug"
+print ("here are the days:", days) #comma will put days in next line
+print ("Here are the months:", months)
+
+print ( """
+There's something going on here.
+With the three double-quotes. 
+We'll be able to type as much as we like
+Even 4 lines if we want, or 5, or 6.
+""" ) # triple quotes to print in multiple lines
