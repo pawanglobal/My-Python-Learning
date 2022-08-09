8fa91a92d4efde5bf0961bf363c2c95a290af197
PAWAN KUMAR <pawanglobalO7@gmail.com>
2022-08-09 21:53:14 +0200

    Numbers and Math in Python

---
+++ ex3.py
@@ -0,0 +1,35 @@
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4 )
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2 )
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+#math operations work according to acronym PEMDAS (parentheses, exponents, multiplication, division, addition, subtraction)
+
+#if you want to find something you can write a program
+
+#You can add (+), subtract (-), multiply (*), and divide (/) integers in Python.
+
+#Python uses two multiplication symbols to represent exponents
+
+#Python calls any number with a decimal point a float.
+
+#How does % work? Another way to say it is, “X divided by Y with J remaining.” For example, “100
+#divided by 16 with 4 remaining.” The result of % is the J part, or the remaining part
