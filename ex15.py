ae7e7981620d76c6524313190393f5227a134e26
PAWAN KUMAR <pawanglobalO7@gmail.com>
2022-08-10 09:55:53 +0200

    Reading Files and Using other files data

---
+++ ex15.py
@@ -0,0 +1,31 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open (filename)
+
+print ("Here's your file %r:" % filename)
+print (txt.read())
+
+print ("Type the filename again:")
+file_again = input(">")
+
+txt_again = open(file_again)
+print (txt_again.read())
+
+#python3
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input(">")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
