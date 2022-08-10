a7ec6e9b87c59a3424a24600cf36e8608e1e79fa
PAWAN KUMAR <pawanglobalO7@gmail.com>
2022-08-10 06:29:16 +0200

    Input (Asking Questions)

---
+++ ex11.py
@@ -0,0 +1,12 @@
+
+print ("How old are you?",) #or use end=' ' to not end the print line and go to the next line
+age = input()
+print ("How tall are you?",)
+height = input()
+print ("How much do you weight?",)
+weight = input ()
+print ("so, you're %r old, %r tall and %r heavy." %(age, height, weight))
+
+#python3
+
+print(f"so, you're {age} old, {height} tall and {weight} heavy.")
